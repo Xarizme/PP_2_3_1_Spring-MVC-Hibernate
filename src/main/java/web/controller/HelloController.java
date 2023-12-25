0a9c98e78eacbc8e85ecc57decbffece0941e3d5
Xarizme <Dodger19@yandex.ru>
2023-12-25 14:10:49 +0300

    Учусь

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -3,7 +3,8 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import web.model.Car;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +20,15 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+	@GetMapping(value = "/car")
+	public String printSumLeghtWord(ModelMap model) {
+		List<Car> carList = new ArrayList<>();
+		carList.add(new Car("BMW", "X5", 2019));
+		carList.add(new Car("Mercedes", "C-class", 2018));
+		carList.add(new Car("Audi", "A8", 2017));
+		model.addAttribute("carList", carList);
+		return "carList";
 	}
 	
 }
